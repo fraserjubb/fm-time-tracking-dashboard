06fdcfad4ca5255924a32abb1883f1f756079229
Fraser Jubb <fraser.jubb@gmail.com>
2025-11-04 19:05:57 +0000

    Made query selectors more streamlined, created an event listener for weekly and moved fetch request into getWeeklyData function

--- app.js
+++ app.js
@@ -1,29 +1,38 @@
-const statTitle = document.querySelectorAll('.stat__title');
-const statTitleList = Array.from(statTitle);
+const statTitleList = Array.from(
+  document.querySelectorAll('.dashboard__stats-activity')
+);
 
-const statCurrent = document.querySelectorAll('.stat__current');
-const statCurrentList = Array.from(statCurrent);
+const statCurrentList = Array.from(
+  document.querySelectorAll('.dashboard__stats-current')
+);
 
-const statPrevious = document.querySelectorAll('.stat__previous');
-const statPreviousList = Array.from(statPrevious);
+const statPreviousList = Array.from(
+  document.querySelectorAll('.dashboard__stats-previous')
+);
 
-console.log(statTitleList[0].textContent);
+const weekly = document.querySelector('#weekly');
 
-fetch('data.json')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`HTTP Error: ${response.status}`);
-    }
-    return response.json();
-  })
-  .then(data =>
-    data.forEach((item, i) => {
-      statTitleList[i].textContent = item.title;
-      statCurrentList[i].textContent = item.timeframes.monthly.current;
-      statPreviousList[i].textContent = item.timeframes.monthly.previous;
+function getWeeklyData() {
+  fetch('data.json')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP Error: ${response.status}`);
+      }
+      return response.json();
     })
-  )
-  .catch(error => console.log(error));
+    .then(data =>
+      data.forEach((item, i) => {
+        statTitleList[i].textContent = item.title;
+        statCurrentList[i].textContent = `${item.timeframes.weekly.current}hrs`;
+        statPreviousList[
+          i
+        ].textContent = `Last Week - ${item.timeframes.weekly.previous}hrs`;
+      })
+    )
+    .catch(error => console.log(error));
+}
+
+weekly.addEventListener('click', getWeeklyData);
 
 // fetch('data.json')
 //   .then(response => {
