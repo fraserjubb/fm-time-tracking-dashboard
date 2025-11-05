921b23100453e367f26a607c689b49f195260fb4
Fraser Jubb <fraser.jubb@gmail.com>
2025-11-05 16:37:45 +0000

    Added fuctions and event listeners for getting data from daily and monthly

--- app.js
+++ app.js
@@ -10,9 +10,43 @@ const statPreviousList = Array.from(
   document.querySelectorAll('.dashboard__stats-previous')
 );
 
-const weekly = document.querySelector('#weekly');
+const dailyTxt = document.querySelector('#daily');
+const weeklyTxt = document.querySelector('#weekly');
+const monthlyTxt = document.querySelector('#monthly');
+
+function getDailyData() {
+  // Style Text
+  weeklyTxt.classList.remove('white-text');
+  monthlyTxt.classList.remove('white-text');
+  dailyTxt.classList.add('white-text');
+
+  // Fetch data
+  fetch('data.json')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP Error: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data =>
+      data.forEach((item, i) => {
+        statTitleList[i].textContent = item.title;
+        statCurrentList[i].textContent = `${item.timeframes.daily.current}hrs`;
+        statPreviousList[
+          i
+        ].textContent = `Yesterday - ${item.timeframes.daily.previous}hrs`;
+      })
+    )
+    .catch(error => console.log(error));
+}
 
 function getWeeklyData() {
+  // Style Text
+  dailyTxt.classList.remove('white-text');
+  monthlyTxt.classList.remove('white-text');
+  weeklyTxt.classList.add('white-text');
+
+  // Fetch data
   fetch('data.json')
     .then(response => {
       if (!response.ok) {
@@ -32,7 +66,37 @@ function getWeeklyData() {
     .catch(error => console.log(error));
 }
 
-weekly.addEventListener('click', getWeeklyData);
+function getMonthlyData() {
+  // Style Text
+  dailyTxt.classList.remove('white-text');
+  weeklyTxt.classList.remove('white-text');
+  monthlyTxt.classList.add('white-text');
+
+  // Fetch data
+  fetch('data.json')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP Error: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data =>
+      data.forEach((item, i) => {
+        statTitleList[i].textContent = item.title;
+        statCurrentList[
+          i
+        ].textContent = `${item.timeframes.monthly.current}hrs`;
+        statPreviousList[
+          i
+        ].textContent = `Last Month - ${item.timeframes.monthly.previous}hrs`;
+      })
+    )
+    .catch(error => console.log(error));
+}
+
+dailyTxt.addEventListener('click', getDailyData);
+weeklyTxt.addEventListener('click', getWeeklyData);
+monthlyTxt.addEventListener('click', getMonthlyData);
 
 // fetch('data.json')
 //   .then(response => {
